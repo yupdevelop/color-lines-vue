47c5e620eddf91dfbc4c00adda51a79aeb579176
yupdevelop <yupdevelop@gmail.com>
2024-06-10 17:42:25 +0800

    migrate to vite and vite-plugin-singlefile

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  plugins: [vue(), viteSingleFile()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
